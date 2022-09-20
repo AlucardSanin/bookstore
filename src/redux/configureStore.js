9b861422900ccf56c7bb8b9a5d60e10a4167621e
AlucardSanin <alucardsanin@gmail.com>
2022-09-20 13:55:20 -0400

    Fix Linter Errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
-import { configureStore, Redux } from '@reduxjs/toolkit'
-import counterReducer from "./books/books";
-import checkCategoryReducer from "./categories/categories";
+import { configureStore, Redux } from '@reduxjs/toolkit';
+import counterReducer from './books/books';
+import checkCategoryReducer from './categories/categories';
 
 const rootReducer = Redux.combineReducers({
   count: counterReducer,
   check: checkCategoryReducer,
 });
 
+// eslint-disable-next-line no-unused-vars
 const store = configureStore({ reducer: rootReducer });
